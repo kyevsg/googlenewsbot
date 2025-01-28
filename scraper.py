25bbad4ac0a20590ac6696c9df58311e32819d14
Kye Stapleton-Gray <88254707+kyevsg@users.noreply.github.com>
2025-01-27 22:39:58 -0500

    fixing scraper

--- scraper.py
+++ scraper.py
@@ -5,10 +5,8 @@ import urllib.request
 def scraper(keyword):
     apikey = ""  # add your api key here
     url = f"https://gnews.io/api/v4/search?q={keyword}&max=4&apikey={apikey}"
-    article_links = []
     with urllib.request.urlopen(url) as response:
         data = json.loads(response.read().decode("utf-8"))
         articles = data["articles"]
-        for i in range(len(articles)):
-            article_links.append(articles[i][url])
+        article_links = [i["url"] for i in articles]
         return article_links
